a927e871e45d0ea297e2e487fd7a0c48c8bca1aa
bures <richardbures@email.cz>
2016-10-02 12:09:10 +0200

    some

--- app/Player.js
+++ app/Player.js
@@ -4,8 +4,8 @@
 
 
 function SHQPlayer(game, cursors) {
-
-    var createPlayer = function () {
+    this.gravity = gravity;
+    var _createPlayer = function () {
         var player = game.add.graphics(stageWidth / 4, stageHeight / 8);
         player.beginFill(0x000000);
         player.drawRect(0, 0, playerWidth, playerHeight);
@@ -13,15 +13,22 @@ function SHQPlayer(game, cursors) {
 
         game.physics.arcade.enable(player);
 
-        player.body.gravity.y = gravity;
+        player.body.gravity.y = this.gravity;
         player.body.collideWorldBounds = true;
         return player;
     };
-    this.player = createPlayer();
+    this.player = _createPlayer();
+    this.isWhiteWorld = true;
 
     this.playerMovement = function () {
-        if (cursors.up.isDown && this.player.body.touching.down) {
+        var jumpKey = this.isWhiteWorld? cursors.up : cursors.down,
+            switchKey = this.isWhiteWorld? cursors.down : cursors.up;
+
+        if (jumpKey.isDown && this.player.body.touching.down) {
             this.player.body.velocity.y = -playerJump;
+        }
+        if(switchKey.isDown){
+            _switchWorldHandler.call(this);
         }
         if (cursors.left.isDown) {
             //  Move to the left
@@ -33,5 +40,9 @@ function SHQPlayer(game, cursors) {
         } else {
             this.player.body.velocity.x = 0;
         }
+    };
+
+    var _switchWorldHandler = function () {
+        this.gravity = -this.gravity;
     };
 }
