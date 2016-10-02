b0f25beab5b81767a216231a3c141e168d3f988c
bures <richardbures@email.cz>
2016-10-02 10:25:08 +0200

    pada

--- app/main.js
+++ app/main.js
@@ -4,12 +4,14 @@
 
 window.onload = function () {
 
+
+
     //  Note that this html file is set to pull down Phaser 2.5.0 from the JS Delivr CDN.
     //  Although it will work fine with this tutorial, it's almost certainly not the most current version.
     //  Be sure to replace it with an updated version before you start experimenting with adding your own code.
 
-    var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-container', {preload: preload, create: create});
-    var player, background;
+    var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-container', {preload: preload, create: create, update:update}, true);
+    var player, backgroundWhite, backgroundBlack;
     function preload() {
 
     }
@@ -17,10 +19,25 @@ window.onload = function () {
     function create() {
         game.stage.backgroundColor = '#000000';
 
-        background = game.add.graphics(0, 0);
-        background.beginFill(0xffffff);
-        background.drawRect(0, 0, 800,300);
-        background.endFill();
+        backgroundWhite = game.add.group();
+        backgroundWhite.enableBody = true;
+        var BCKW1 = game.add.graphics(0,0);
+        BCKW1.beginFill(0xffffff);
+        BCKW1.drawRect(0, 0, 800,300);
+        BCKW1.endFill();
+        backgroundWhite.add(BCKW1);
+        BCKW1.body.immovable = true;
+
+        backgroundBlack = game.add.group();
+        backgroundBlack.enableBody = true;
+        var BCKB1 = game.add.graphics(0,300);
+        BCKB1.beginFill(0x000000);
+        BCKB1.drawRect(0, 0, 800,300);
+        BCKB1.endFill();
+        backgroundBlack.add(BCKB1);
+        BCKB1.body.immovable = true;
+
+
 
         game.physics.startSystem(Phaser.Physics.ARCADE);
 
@@ -35,4 +52,7 @@ window.onload = function () {
         player.body.collideWorldBounds = true;
     }
 
+    function update() {
+        var hitPlatform = game.physics.arcade.collide(player, backgroundBlack);
+    }
 };
