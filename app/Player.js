b6cb0067617fb9897b8d3ccb2abd7d44a5068365
bures <richardbures@email.cz>
2016-10-04 12:23:43 +0200

    player switching done

--- app/Player.js
+++ app/Player.js
@@ -2,35 +2,26 @@
  * Created by rockuo on 2.10.16.
  */
 
+var isAllowedToJump = true;
 
 function SHQPlayer(game, cursors) {
     this.gravity = gravity;
-    var _createPlayer = function () {
-        var player = game.add.graphics(stageWidth / 4, stageHeight / 8);
-        player.beginFill(0x000000);
-        player.drawRect(0, 0, playerWidth, playerHeight);
-        player.endFill();
-
-        game.physics.arcade.enable(player);
-
-        player.body.gravity.y = this.gravity;
-        player.body.collideWorldBounds = true;
-        return player;
-    };
-    this.player = _createPlayer();
+    this.player = _createPlayer(game);
     this.isWhiteWorld = true;
 
     this.playerMovement = function () {
-        var jumpKey = this.isWhiteWorld? cursors.up : cursors.down,
-            switchKey = this.isWhiteWorld? cursors.down : cursors.up;
+        var jumpKey = this.isWhiteWorld ? cursors.up : cursors.down,
+            switchKey = this.isWhiteWorld ? cursors.down : cursors.up;
 
+        if (switchKey.isDown) {
+            isAllowedToJump = false;
+            _switchWorldHandler(this);
+        } else if (jumpKey.isDown && isAllowedToJump && this.player.body.touching[this.isWhiteWorld ? 'down' : 'up']) {
+            this.player.body.velocity.y = this.isWhiteWorld ? -playerJump : playerJump;
+        } else if (!isAllowedToJump && !jumpKey.isDown) {
+            isAllowedToJump = true;
+        }
 
-        if (jumpKey.isDown && this.player.body.touching[this.isWhiteWorld?'down':'up']) {
-            this.player.body.velocity.y = this.isWhiteWorld?-playerJump:playerJump;
-        }
-        if(switchKey.isDown){
-            _switchWorldHandler.call(this);
-        }
         if (cursors.left.isDown) {
             //  Move to the left
             this.player.body.velocity.x = -playerSpeed;
@@ -42,18 +33,32 @@ function SHQPlayer(game, cursors) {
             this.player.body.velocity.x = 0;
         }
     };
+}
 
-    var _switchWorldHandler = function () {
-        this.gravity = -this.gravity;
 
-        var newY = this.isWhiteWorld? stageHeight/2 - (this.player.body.y + playerHeight) + stageHeight/2 :
-            stageHeight/2 - (this.player.body.y - stageHeight/2) -playerHeight ;
+function _createPlayer(game) {
+    var player = game.add.graphics(stageWidth / 4, stageHeight / 8);
+    player.beginFill(0x000000);
+    player.drawRect(0, 0, playerWidth, playerHeight);
+    player.endFill();
 
-        this.isWhiteWorld = !this.isWhiteWorld;
-        this.player.body.gravity.y = this.gravity;
-        this.player.body.y = newY;
-        this.player.beginFill(this.isWhiteWorld?0x000000:0xffffff);
-        this.player.drawRect(0, 0, playerWidth, playerHeight);
-        this.player.endFill();
-    };
+    game.physics.arcade.enable(player);
+
+    player.body.gravity.y = gravity;
+    player.body.collideWorldBounds = true;
+    return player;
+}
+
+function _switchWorldHandler(instance) {
+    instance.gravity = -instance.gravity;
+
+    var newY = instance.isWhiteWorld ? stageHeight / 2 - (instance.player.body.y + playerHeight) + stageHeight / 2 :
+    stageHeight / 2 - (instance.player.body.y - stageHeight / 2) - playerHeight;
+
+    instance.isWhiteWorld = !instance.isWhiteWorld;
+    instance.player.body.gravity.y = instance.gravity;
+    instance.player.body.y = newY;
+    instance.player.beginFill(instance.isWhiteWorld ? 0x000000 : 0xffffff);
+    instance.player.drawRect(0, 0, playerWidth, playerHeight);
+    instance.player.endFill();
 }
